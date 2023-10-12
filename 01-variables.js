5696431ce9d6a0ac385bd9c8aab88fddf43b8731
jmarribas <jomuarribas@gmail.com>
2023-10-12 12:34:56 +0200

    create variables session 01

---
+++ 01-variables.js
@@ -0,0 +1,17 @@
+var greetings = "hola desde";
+var school = "pmba";
+
+var numberOne = 20;
+var numberTwo = 30;
+
+var greetingsFromSchool = greetings + " " + school;
+var finalNumber = numberOne / numberTwo;
+
+var myBooleat = false
+
+var myObject = {
+  name: "Ignacio",
+  surname: "Diez"
+};
+
+console.log(greetingsFromSchool + " " + finalNumber + " " + myObject.surname);
